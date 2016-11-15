7a4f2a9a531c6af7324eb07b07b7ae5fcaae64ac
schneider-IMA15 <lukas.schneider@edu.fh-joanneum.at>
2016-11-15 12:42:48 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Lukas Schneider on 15.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Boolean vanessaCold = true;
+
+        while (vanessaCold){
+            double i = Math.PI;
+            System.out.println(i*System.currentTimeMillis());
+        }
+    }
+}
